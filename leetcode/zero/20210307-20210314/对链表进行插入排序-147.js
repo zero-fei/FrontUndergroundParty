6255d44a3bef7d8530df97846ba13a22b9bc308e
曾龙飞 <805040010@qq.com>
2021-03-21 20:35:43 +0800

    feat: 提交代码

--- leetcode/zero/20210307-20210314/对链表进行插入排序-147.js
+++ leetcode/zero/20210307-20210314/对链表进行插入排序-147.js
@@ -19,14 +19,15 @@ var insertionSortList = function (head) {
       lastSorted = lastSorted.next; // 将lastSorted后移一位,curr变成新的lastSorted
     } else {
       let prev = dummyHead; // 从链表头开始遍历 prev是插入节点curr位置的前一个节点
-      while (prev.next.val <= curr.val) {
+      while (prev.next.val <= curr.val) { // 循环退出的条件是找到curr应该插入的位置
         prev = prev.next;
       }
+       // 以下三行是为了完成对curr的插入
       lastSorted.next = curr.next;
       curr.next = prev.next;
       prev.next = curr;
     }
     curr = lastSorted.next;
-  }
+  }       
   return dummyHead.next
 };
