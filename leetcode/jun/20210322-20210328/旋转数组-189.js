7378626b3308a9e88503cf6a550ff97ab85efcd3
June_z <453918836@qq.com>
2021-03-28 01:23:22 +0800

    feat(20210322-20210328): 补充说明

--- leetcode/jun/20210322-20210328/旋转数组-189.js
+++ leetcode/jun/20210322-20210328/旋转数组-189.js
@@ -17,14 +17,14 @@
  * 方法二： splice
  * 方法三： 原数组内部交换
  *
- * 总结：方法一比较好理解，内存相对较少，耗时比较长
- *      方法三内存跟方法一消耗差不多，耗时稍微少一点，但是比较难理解
- *      方法二时间消耗最少，但是空间使用也是最多，传说中的牺牲空间换时间
+ * 总结：方法一比较好理解，内存相对较少，耗时指数级增长
+ *      方法三内存跟方法一消耗差不多，耗时相对稳定，操作时间在数量大的时候优势比较明显
+ *      方法二时间消耗最少，但是空间使用也是最多，在k为50w的时候有内存报错，故在数量级少的时候比较合适
  */
 
 
 /**
- * 方法一
+ * 方法一  0.139ms
  *
  * @param {number[]} nums
  * @param {number} k
@@ -53,12 +53,15 @@ var rotate = function(nums, k) {
  * 内存消耗：40.1 MB, 在所有 JavaScript 提交中击败了12.13%的用户
  */
 var rotate2 = function(nums, k) {
+  k = k % nums.length
+  console.log(k, nums.length)
   const rightList = nums.splice(nums.length - k, k)
   nums.splice(0, 0, ...rightList)
 };
 
+const gcd = (x, y) => y ? gcd(y, x % y) : x;
 /**
- * 方法三
+ * 方法三 0.306ms
  *
  * @param {number[]} nums
  * @param {number} k
@@ -70,7 +73,6 @@ var rotate2 = function(nums, k) {
 var rotate3 = function(nums, k) {
   const len = nums.length
   let right = k % len
-  const gcd = (x, y) => y ? gcd(y, x % y) : x;
   let counter = gcd(right, len);
   for (let start = 0; start < counter; ++start) {
     let current = start;
@@ -85,9 +87,25 @@ var rotate3 = function(nums, k) {
   }
 };
 
-var test = [-1,-100,3,99]
-var test1 = [1,2,3,4,5,6,7]
-var test2 = [1,2,3]
-rotate3(test, 2)
-console.log(test)
+const k = 500000
+var test0 = new Array(Math.floor(Math.random() * 1000000)).fill(Math.floor(Math.random() * 100))
+var test1 = new Array(Math.floor(Math.random() * 1000000)).fill(Math.floor(Math.random() * 100))
+var test2 = new Array(Math.floor(Math.random() * 1000000)).fill(Math.floor(Math.random() * 100))
+// console.time('counter1')
+// rotate(test0, k)
+// rotate(test1, k)
+// rotate(test2, k)
+// console.timeEnd('counter1')
+
+// console.time('counter2')
+// rotate2(test0, k)
+// rotate2(test1, k)
+// rotate2(test2, k)
+// console.timeEnd('counter2')
+
+console.time('counter3')
+rotate3(test0, k)
+rotate3(test1, k)
+rotate3(test2, k)
+console.timeEnd('counter3')
 
