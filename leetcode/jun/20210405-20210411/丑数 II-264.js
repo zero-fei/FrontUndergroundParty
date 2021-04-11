a912f886509fa03c491730b237b8cfd7e01351d0
June_z <453918836@qq.com>
2021-04-11 17:16:59 +0800

    feat(20210405-20210411): leetcode 每周两题

---
+++ leetcode/jun/20210405-20210411/丑数 II-264.js
@@ -0,0 +1,36 @@
+/**
+ * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+ *
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ *
+ * 解法：从2开始，每次生成当前最小的丑数集合，取最小值
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function(n) {
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = 1;
+  let p2 = 1, p3 = 1, p5 = 1;
+  for (let i = 2; i <= n; i++) {
+    const num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+    dp[i] = Math.min(Math.min(num2, num3), num5);
+    if (dp[i] === num2) {
+      p2++;
+    }
+    if (dp[i] === num3) {
+      p3++;
+    }
+    if (dp[i] === num5) {
+      p5++;
+    }
+  }
+  return dp[n];
+};
+
+const test = [10, 1] // 12, 1
+test.forEach(item => {
+  console.log(nthUglyNumber(item))
+})
